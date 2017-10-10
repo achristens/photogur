cf3e833b6e6ab3ec47f518ce8df87018dd32753d
Abby Christens <achristens@gmail.com>
2017-10-10 19:11:05 -0400

    Editing pictures

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -26,5 +26,22 @@ class ImagesController < ApplicationController
     end
   end
 
+  def edit
+    @image = Image.find(params[:id])
+  end
+
+  def update
+    @image = Image.find(params[:id])
+
+    @image.title = params[:image][:title]
+    @image.dog_name = params[:image][:dog_name]
+    @image.url = params[:image][:url]
+
+    if @image.save
+      redirect_to "/images/#{@image.id}"
+    else
+      render :edit
+    end
+  end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,11 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root 'images#index'
-  
+
   get 'images'     => 'images#index'
+  
+  get 'images/:id/edit' => 'images#edit'
+  patch 'images/:id' => 'images#update'
 
   post 'images'    => 'images#create'
   get 'images/new' => 'images#new'
