86c77fc44ca0922a2bcc2bb0726460b4e7fc1037
Abby Christens <achristens@gmail.com>
2017-10-10 18:44:33 -0400

    Adding form for adding new pictures

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -6,6 +6,25 @@ class ImagesController < ApplicationController
 
   def show
     @image = Image.find(params[:id])
-
   end
+
+  def new
+    @image = Image.new
+  end
+
+  def create
+    @image = Image.new
+
+    @image.title = params[:image][:title]
+    @image.dog_name = params[:image][:dog_name]
+    @image.url = params[:image][:url]
+
+    if @image.save
+      redirect_to '/images'
+    else
+      render :new
+    end
+  end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  get 'images' => 'images#index'
+  get 'images'     => 'images#index'
+
+  post 'images'    => 'images#create'
+  get 'images/new' => 'images#new'
 
   get 'images/:id' => 'images#show'
-
 end
