aabf7167e8c9e1339f0a05a261953227d045fe65
Abby Christens <achristens@gmail.com>
2017-10-10 18:16:38 -0400

    Displaying pictures with title and name on images page

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+  Image.create(
+    dog_name: "Rupert",
+    title: "Playing in the grass",
+    url: "http://loremflickr.com/320/240?random=1"
+  )
+
+  Image.create(
+    dog_name: "Phyllis",
+    title: "Having fun",
+    url: "http://loremflickr.com/320/240?random=2"
+  )
+
+  Image.create(
+    dog_name: "Snuffles",
+    title: "Loving life",
+    url: "http://loremflickr.com/320/240?random=3"
+  )
