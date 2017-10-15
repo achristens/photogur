d424a14386372be6e7201ca89bdc024d0ec02553
Abby Christens <achristens@gmail.com>
2017-10-15 16:24:24 -0400

    Adding year-specific section

--- app/models/image.rb
+++ app/models/image.rb
@@ -8,9 +8,11 @@ class Image < ApplicationRecord
     Image.newest_first.limit(5)
   end
 
-
   def self.created_before(time)
     Image.where("created_at < ?", time)
   end
 
+  def self.pictures_created_in_year(select_year)
+    Image.where("cast(strftime('%Y', created_at) as int) = ?", select_year)
+  end
 end
