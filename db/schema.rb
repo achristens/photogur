52f2775d43c7d890b090b9224c5f63832d499f3b
Abby Christens <achristens@gmail.com>
2017-10-19 14:45:55 -0400

    Adding new user for, controller, views, model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171017153127) do
+ActiveRecord::Schema.define(version: 20171019182908) do
 
   create_table "comments", force: :cascade do |t|
     t.string   "name"
@@ -37,6 +37,13 @@ ActiveRecord::Schema.define(version: 20171017153127) do
     t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "email"
+    t.string   "password_digest"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
   end
 
 end
