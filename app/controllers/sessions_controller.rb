4f23bee81cba469883af02f80ee84db3b4a8f85c
Abby Christens <achristens@gmail.com>
2017-10-19 14:59:53 -0400

    Adding sessions controller, logging in with success message

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,8 +4,17 @@ class SessionsController < ApplicationController
   end
 
   def create
+    user = User.find_by(email: params[:email])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect_to images_url, notice: "Logged in!"
+    else
+      render :new
+    end
   end
 
   def destroy
+    session[:user_id] = nil
+    redirect_to images_url, notice: "Logged out!"
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,8 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
-
-  get 'sessions/create'
-
-  get 'sessions/destroy'
-
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   resource :users, only: %i(create new)
+  resource :sessions, only: %i(create new destroy)
 
   root 'images#index'
 
